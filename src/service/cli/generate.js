353eccfb2019b27ce3297f65d392da089173ba27
Poly Titova <poly_titova@mail.ru>
2020-06-30 10:03:13 +0300

    Adding colors

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// const chalk = require(`chalk`);
+const chalk = require(`chalk`);
 const fs = require(`fs`);
 const { ExitCode } = require(`../constants`);
 const {
@@ -39,25 +39,25 @@ const generateOffers = count =>
       category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]]
     }));
 
-// const makeMockData = (filename, content) => {
-//   fs.writeFile(filename, content, (err) => {
-//     if (err) {
-//       return console.error(`Can't write data to file`);
-//     }
-
-//     return console.log(`The file has been saved!`);
-//   });
-// };
-
-const makeMockData = async (filename, content) => {
+const makeMockData = (filename, content) => {
   fs.writeFile(filename, content, (err) => {
-    try {
-      return console.log(`The file has been saved!`);
-    } catch (err) {
-      return console.error(`Can't write data to file`);
+    if (err) {
+      return console.error(chalk.red(`Can't write data to file`));
     }
+
+    return console.log(chalk.green(`The file has been saved!`));
   });
 };
+
+// const makeMockData = async (filename, content) => {
+//   fs.writeFile(filename, content, (err) => {
+//     try {
+//       return console.log(`The file has been saved!`);
+//     } catch (err) {
+//       return console.error(`Can't write data to file`);
+//     }
+//   });
+// };
 
 module.exports = {
   name: `--generate`,
@@ -65,7 +65,7 @@ module.exports = {
     const [count] = userIndex;
 
     if (count > MAX_COUNT) {
-      console.error(`Не больше ${MAX_COUNT} объявлений`);
+      console.error(chalk.red(`Не больше ${MAX_COUNT} объявлений`));
       process.exit(ExitCode.fail);
     }
 
--- src/service/cli/help.js
+++ src/service/cli/help.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// const chalk = require(`chalk`);
+const chalk = require(`chalk`);
 const OUTPUT = `
 Программа запускает http-сервер и формирует файл с данными для API.
 
@@ -16,6 +16,6 @@ const OUTPUT = `
 module.exports = {
   name: `--help`,
   run() {
-    console.info(OUTPUT);
+    console.info(chalk.gray(OUTPUT));
   }
 };
