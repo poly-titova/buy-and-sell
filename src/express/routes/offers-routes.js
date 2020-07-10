d026aa731be9f8301547829c70d751ad4f0796bf
Poly Titova <poly_titova@mail.ru>
2020-07-10 13:25:09 +0300

    Определяем  маршруты и в качестве ответа отправляем путь маршрута

---
+++ src/express/routes/offers-routes.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Подключаем и инициализируем экземпляр Router
+const {Router} = require(`express`);
+const offersRouter = new Router();
+
+// Определяем `GET` маршруты.
+// В качестве ответа отправляем путь маршрута.
+// Следует помнить, что в первом параметре мы указываем путь маршрута
+// без `offers`, т.к. уже указали при подключении модуля маршрута
+// в `index.js`.
+offersRouter.get(`/category/:id`, (req, res) => res.send(`/offers/category/:id`));
+offersRouter.get(`/add`, (req, res) => res.send(`/offers/add`));
+offersRouter.get(`/edit/:id`, (req, res) => res.send(`/offers/edit/:id`));
+offersRouter.get(`/:id`, (req, res) => res.send(`/offers/:id`));
+
+module.exports = offersRouter;
