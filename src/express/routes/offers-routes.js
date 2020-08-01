1c8293f2971e84363fd8cac433ddbeea084a1994
Poly Titova <poly_titova@mail.ru>
2020-08-01 11:37:34 +0300

    Сделали страницу для редактирования объявления

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -11,7 +11,7 @@ const offersRouter = new Router();
 // в `index.js`.
 offersRouter.get(`/category/:id`, (req, res) => res.render(`category`));
 offersRouter.get(`/add`, (req, res) => res.render(`new-ticket`));
-offersRouter.get(`/edit/:id`, (req, res) => res.send(`/offers/edit/:id`));
+offersRouter.get(`/edit/:id`, (req, res) => res.render(`ticket-edit`));
 offersRouter.get(`/:id`, (req, res) => res.send(`/offers/:id`));
 
 module.exports = offersRouter;
