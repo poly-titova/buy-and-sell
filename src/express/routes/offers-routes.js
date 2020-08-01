045b401411b50c3de2d034ad114d49d951c0b642
Poly Titova <poly_titova@mail.ru>
2020-08-01 11:30:57 +0300

    Сделали страницу с объявлениями в отдельной категории

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -9,7 +9,7 @@ const offersRouter = new Router();
 // Следует помнить, что в первом параметре мы указываем путь маршрута
 // без `offers`, т.к. уже указали при подключении модуля маршрута
 // в `index.js`.
-offersRouter.get(`/category/:id`, (req, res) => res.send(`/offers/category/:id`));
+offersRouter.get(`/category/:id`, (req, res) => res.render(`category`));
 offersRouter.get(`/add`, (req, res) => res.send(`/offers/add`));
 offersRouter.get(`/edit/:id`, (req, res) => res.send(`/offers/edit/:id`));
 offersRouter.get(`/:id`, (req, res) => res.send(`/offers/:id`));
