87fe0acdc6ae7ca03bb6e67568521d5981dfa3b5
Poly Titova <poly_titova@mail.ru>
2020-10-31 19:34:50 +0300

    Создали сервис, который создаёт новое объявление

--- src/service/api/offer.js
+++ src/service/api/offer.js
@@ -2,6 +2,7 @@
 
 const { Router } = require(`express`);
 const { HttpCode } = require(`../constants`);
+const offerValidator = require(`../middlewares/offer-validator`);
 
 const route = new Router();
 
@@ -10,6 +11,10 @@ module.exports = (app, offerService) => {
 
   // возвращает список объявлений
   route.get(`/`, (req, res) => {
+    // пользуемся возможностями сервиса offerService,
+    // который передаётся в виде аргумента
+    // вызываем метод findAll, который должен 
+    // вернуть все объявления
     const offers = offerService.findAll();
     res.status(HttpCode.OK).json(offers);
   });
@@ -32,6 +37,18 @@ module.exports = (app, offerService) => {
     }
 
     return res.status(HttpCode.OK)
+      .json(offer);
+  });
+
+  // создаёт новое объявление
+  route.post(`/`, offerValidator, (req, res) => {
+    // пользуемся возможностями сервиса offerService,
+    // который передаётся в виде аргумента
+    // вызываем метод create, который должен 
+    // создаёт новое объявление
+    const offer = offerService.create(req.body);
+
+    return res.status(HttpCode.CREATED)
       .json(offer);
   });
 }
--- src/service/data-service/offer.js
+++ src/service/data-service/offer.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const {nanoid} = require(`nanoid`);
+const {MAX_ID_LENGTH} = require(`../constants`)
+
 class OfferService {
   // конструктор принимает данные о всех объявлениях 
   // и сохраняет их в одноимённое приватное свойство
@@ -15,6 +18,16 @@ class OfferService {
   // метод который получает данные только для определённого объявления
   findOne(id) {
     return this._offers.find((item) => item.id === id);
+  }
+
+  // метод который создаёт новое объявление
+  // полученные данные мы просто добавляем в массив — хранилище
+  create(offer) {
+    const newOffer = Object
+      .assign({id: nanoid(MAX_ID_LENGTH), comments: []}, offer);
+
+    this._offers.push(newOffer);
+    return newOffer;
   }
 };
 
