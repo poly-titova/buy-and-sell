6382e74247cd5b847753d6eaa8e56b254ed2a053
Poly Titova <poly_titova@mail.ru>
2021-01-15 17:19:29 +0300

    Реализовали методы

--- src/express/api.js
+++ src/express/api.js
@@ -14,4 +14,28 @@ class API {
     const response = await this._http.request({url, ...options});
     return response.data;
   }
+
+  // методы для получения и отправки данных
+  getOffers(){
+    return this._load('/offers'); 
+  }
+
+  getOffer(id){
+    return this._load(`/offers/${id}`);
+  }
+
+  search(query){
+    return this._load(`/search`, {params: {query}});
+  }
+
+  async getCategories(){
+    return this._load('/category');
+  }
+
+  async createOffer(data){
+    return this._load(`/offers`, {
+      method: `POST`,
+      data
+    });
+  }
 }
