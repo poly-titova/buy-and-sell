0e47fa3d812096b8bbfb775ac42f79002daeb2a2
Poly Titova <poly_titova@mail.ru>
2021-01-18 21:45:48 +0300

    Сделали показ списка всех объявлений

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -1,12 +1,15 @@
 'use strict';
 
 // Подключаем и инициализируем экземпляр Router
-const {Router} = require(`express`);
+const { Router } = require(`express`);
 const mainRoutes = new Router();
 
 // Определяем `GET` маршруты.
 // В качестве ответа отправляем путь маршрута.
-mainRoutes.get(`/`, (req, res) => res.render(`main`));
+mainRoutes.get(`/`, async (req, res) => {
+  const offers = await api.getOffers();
+  res.render(`main`, { offers });
+});
 mainRoutes.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRoutes.get(`/login`, (req, res) => res.render(`login`));
 mainRoutes.get(`/search`, (req, res) => res.render(`search-result`));
