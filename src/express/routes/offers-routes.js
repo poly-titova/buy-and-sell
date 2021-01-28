808b365dea9b410f6851f89a919124b1108fa1bb
Poly Titova <poly_titova@mail.ru>
2021-01-28 21:26:22 +0300

    Сделали редактирование объявлений

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Подключаем и инициализируем экземпляр Router
-const {Router} = require(`express`);
+const { Router } = require(`express`);
 const offersRouter = new Router();
 
 // Определяем `GET` маршруты.
@@ -11,7 +11,16 @@ const offersRouter = new Router();
 // в `index.js`.
 offersRouter.get(`/category/:id`, (req, res) => res.render(`category`));
 offersRouter.get(`/add`, (req, res) => res.render(`new-ticket`));
-offersRouter.get(`/edit/:id`, (req, res) => res.render(`ticket-edit`));
+
+offersRouter.get(`/edit/:id`, async (req, res) => {
+  const {id} = req.params;
+  const [offer, categories] = await Promise.all([
+    api.getOffer(id),
+    api.getCategories()
+  ]);
+  res.render(`offers/ticket-edit`, {offer, categories});
+});
+
 offersRouter.get(`/:id`, (req, res) => res.render(`ticket`));
 
 module.exports = offersRouter;
