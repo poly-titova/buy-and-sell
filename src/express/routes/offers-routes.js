9292994b547eeed45a1ee5a7ed5289a50c4c727e
Poly Titova <poly_titova@mail.ru>
2021-02-13 21:17:02 +0300

    Сделали добавление обьявления

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -7,7 +7,7 @@ const api = require(`../api`).getAPI();
 
 const multer = require(`multer`);
 const path = require(`path`);
-const {nanoid} = require(`nanoid`);
+const { nanoid } = require(`nanoid`);
 
 const UPLOAD_DIR = `../upload/img/`;
 
@@ -29,40 +29,38 @@ const storage = multer.diskStorage({
 // без `offers`, т.к. уже указали при подключении модуля маршрута
 // в `index.js`.
 offersRouter.get(`/category/:id`, (req, res) => res.render(`category`));
-offersRouter.get(`/add`, (req, res) => res.render(`new-ticket`));
+offersRouter.get(`/add`, async (req, res) => {
+  const categories = await api.getCategories();
+  res.render(`new-ticket`, { categories });
+});
 
-const upload = multer({storage});
+const upload = multer({ storage });
 
-offersRouter.post(`/add`, 
-  upload.single(`avatar`), //а вот и middleware
-  async (req, res) => {
-
-    // в body содержатся текстовые данные формы, а в file - данные о сохранённом файле
-    const {body, file} = req;
-    const offerData = {
-      picture: file.filename,
-      sum: body.price,
-      type: body.action,
-      description: body.comment,
-      title: body[`ticket-name`],
-      category: body.category
-    };
-    try {
-      await api.post(`/offers`, offerData);
-      res.redirect(`/my`);
-    } catch (e) {
-      res.redirect(`back`);
-    }
+offersRouter.post(`/add`, upload.single(`avatar`), async (req, res) => {
+  const { body, file } = req;
+  const offerData = {
+    picture: file.filename,
+    sum: body.price,
+    type: body.action,
+    description: body.comment,
+    title: body[`ticket-name`],
+    category: body.category
+  };
+  try {
+    await api.createOffer(offerData);
+    res.redirect(`/my`);
+  } catch (error) {
+    res.redirect(`back`);
   }
-);
+});
 
 offersRouter.get(`/edit/:id`, async (req, res) => {
-  const {id} = req.params;
+  const { id } = req.params;
   const [offer, categories] = await Promise.all([
     api.getOffer(id),
     api.getCategories()
   ]);
-  res.render(`ticket-edit`, {offer, categories});
+  res.render(`ticket-edit`, { offer, categories });
 });
 
 offersRouter.get(`/:id`, (req, res) => res.render(`ticket`));
