bdc52416d61f817ee2a645f18d70c73737262599
Poly Titova <poly_titova@mail.ru>
2021-03-27 15:14:00 +0300

    Получили список категорий для которых создано минимум одно объявление

--- queries.sql
+++ queries.sql
@@ -1,2 +1,8 @@
 -- запрос для получения списка всех категорий
 SELECT * FROM categories
+
+-- запрос для получения списка непустых категорий
+SELECT id, name FROM categories
+  JOIN offer_categories
+  ON id = category_id
+  GROUP BY id
