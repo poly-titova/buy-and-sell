a726161d2aa6fda6ae315b9b67397d4aece474a5
Poly Titova <poly_titova@mail.ru>
2021-04-22 21:12:02 +0300

    Изменили структуру файла категорий

--- src/service/data-service/category.js
+++ src/service/data-service/category.js
@@ -1,21 +1,12 @@
 "use strict";
 
 class CategoryService {
-  // конструктор класса принимает один аргумент — offers,
-  // тот самый массив информации с объявлениями
-  constructor(offers) {
-    this._offers = offers;
+  constructor(sequelize) {
+    this._Category = sequelize.models.Category;
   }
 
-  // формирование массива всех категорий для которых есть объявления
   findAll() {
-    const categories = this._offers.reduce((acc, offer) => {
-      acc.add(...offer.category);
-      return acc;
-      // сохраняем лишь уникальные элементы
-    }, new Set());
-
-    return [...categories];
+    return this._Category.findAll({raw: true});
   }
 }
 
