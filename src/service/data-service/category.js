9aece5c9e27ab83677e0a7900fd0114b5f49a4de
Poly Titova <poly_titova@mail.ru>
2021-04-23 21:10:52 +0300

    Получили объявления с комментариями

--- src/service/data-service/category.js
+++ src/service/data-service/category.js
@@ -1,12 +1,39 @@
 "use strict";
 
+const Sequelize = require(`sequelize`);
+const Aliase = require(`../models/aliase`);
+
 class CategoryService {
   constructor(sequelize) {
     this._Category = sequelize.models.Category;
+    this._OfferCategory = sequelize.models.OfferCategory;
   }
 
-  findAll() {
-    return this._Category.findAll({raw: true});
+  async findAll(needCount) {
+    if (needCount) {
+      const result = await this._Category.findAll({
+        attributes: [
+          `id`,
+          `name`,
+          [
+            Sequelize.fn(
+                `COUNT`,
+                `*`
+            ),
+            `count`
+          ]
+        ],
+        group: [Sequelize.col(`Category.id`)],
+        include: [{
+          model: this._OfferCategory,
+          as: Aliase.OFFER_CATEGORIES,
+          attributes: []
+        }]
+      });
+      return result.map((it) => it.get());
+    } else {
+      return this._Category.findAll({raw: true});
+    }
   }
 }
 
