6b86a44c4e7df114196af5bbad04171821be0d4e
Poly Titova <poly_titova@mail.ru>
2021-05-03 13:38:05 +0300

    Прописали комментарии к логике пагинации

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -9,28 +9,35 @@ const OFFERS_PER_PAGE = 8;
 
 // Определяем `GET` маршруты.
 // В качестве ответа отправляем путь маршрута.
-mainRoutes.get(`/`, async (req, res) => {
-  let {page = 1} = req.query;
+mainRouter.get(`/`, async (req, res) => {
+  // получаем номер страницы
+  let { page = 1 } = req.query;
   page = +page;
-  const limit = OFFERS_PER_PAGE;
-  const offset = (page - 1) * OFFERS_PER_PAGE;
 
+  // количество запрашиваемых объявлений равно количеству объявлений на странице
+  const limit = OFFERS_PER_PAGE;
+
+  // количество объявлений, которое нам нужно пропустить - это количество объявлений на предыдущих страницах
+  const offset = (page - 1) * OFFERS_PER_PAGE;
   const [
-    {count, pugOffers},
+    { count, offers },
     categories
   ] = await Promise.all([
-    api.getOffers({limit, offset}),
+    api.getOffers({ limit, offset }),
     api.getCategories(true)
   ]);
 
+  // количество страниц — это общее количество объявлений, поделённое на количество объявлений на странице (с округлением вверх)
   const totalPages = Math.ceil(count / OFFERS_PER_PAGE);
-  res.render(`main`, {pugOffers, page, totalPages, categories});
+
+  // передадим все эти данные в шаблон
+  res.render(`main`, { offers, categories, page, totalPages });
 });
 mainRoutes.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRoutes.get(`/login`, (req, res) => res.render(`login`));
 mainRoutes.get(`/search`, async (req, res) => {
   try {
-    const {search} = req.query;
+    const { search } = req.query;
     const results = await api.search(search);
 
     res.render(`search-result`, {
