01bd67fb252ea9d7b321ef8324e3c861ea3bf6f6
Poly Titova <poly_titova@mail.ru>
2021-05-29 12:05:25 +0300

    Написали валидационую схему

---
+++ src/service/lib/schema.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const Joi = require(`joi`);
+
+module.exports = Joi.object({
+  title: Joi.string()
+    .min(10)
+    .max(100)
+    .required(),
+  
+  picture: Joi.string()
+    .pattern(`jpg`, `png`)
+    .required(),
+
+  description: Joi.string()
+    .min(50)
+    .max(1000)
+    .required(),
+
+  type: Joi.string()
+    .valid(`offer`, `sale`)
+    .required(),
+
+  sum: Joi.number()
+    .min(100)
+    .required(),
+
+  category: Joi.string()
+    .items(Joi.string())
+    .min(1)
+    .required(),
+
+  comments: Joi.object({
+    text: Joi.string()
+      .min(20)
+      .required(),
+  }).required(),
+});
