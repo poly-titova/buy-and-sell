8bd3042b131490c8c52e5304a8cfa5e243f5a2ef
Poly Titova <poly_titova@mail.ru>
2021-05-29 12:07:25 +0300

    Добавили в middlewares валидации объявления обработку схемы

--- src/service/middlewares/offer-validator.js
+++ src/service/middlewares/offer-validator.js
@@ -3,11 +3,29 @@
 const {HttpCode} = require(`../constants`);
 
 const offerKeys = [`category`, `description`, `picture`, `title`, `type`, `sum`];
+const HTTP_CODE_BAD_REQUEST = 400;
 
-module.exports = (req, res, next) => {
+module.exports = (req, res, next, schema) => {
   const newOffer = req.body;
   const keys = Object.keys(newOffer);
   const keysExists = offerKeys.every((key) => keys.includes(key));
+
+  async (req, res, next) => {
+    const { body } = req;
+
+    try {
+      await schema.validateAsync(body, { abortEarly: false });
+    } catch (err) {
+      const { details } = err;
+      res.status(HTTP_CODE_BAD_REQUEST).json({
+        message: details.map((errorDescription) => errorDescription.message),
+        data: body
+      });
+      return;
+    }
+
+    next();
+  }
 
   if (!keysExists) {
     res.status(HttpCode.BAD_REQUEST)
