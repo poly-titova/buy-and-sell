bb372c83c03edd28847d3e999eb35c14f5b036b3
Poly Titova <poly_titova@mail.ru>
2021-06-21 17:08:51 +0300

    Сделали технические поправки

--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -38,6 +38,7 @@ offersRouter.get(`/add`, async (req, res) => {
 const upload = multer({ storage });
 
 offersRouter.post(`/add`, upload.single(`avatar`), async (req, res) => {
+  const { user } = req.session;
   const { body, file } = req;
   const offerData = {
     picture: file.filename,
@@ -45,7 +46,8 @@ offersRouter.post(`/add`, upload.single(`avatar`), async (req, res) => {
     type: body.action,
     description: body.comment,
     title: body[`ticket-name`],
-    categories: body.category
+    categories: ensureArray(body.category),
+    userId: user.id
   };
   try {
     await api.createOffer(offerData);
@@ -57,17 +59,51 @@ offersRouter.post(`/add`, upload.single(`avatar`), async (req, res) => {
 
 offersRouter.get(`/edit/:id`, async (req, res) => {
   const { id } = req.params;
+  const { error } = req.query;
   const [offer, categories] = await Promise.all([
     api.getOffer(id),
     api.getCategories()
   ]);
-  res.render(`ticket-edit`, { offer, categories });
+  res.render(`offers/ticket-edit`, { id, offer, categories, error });
+});
+
+offersRouter.post(`/edit/:id`, upload.single(`avatar`), async (req, res) => {
+  const { body, file } = req;
+  const { id } = req.params;
+  const offerData = {
+    picture: file ? file.filename : body[`old-image`],
+    sum: body.price,
+    type: body.action,
+    description: body.comment,
+    title: body[`ticket-name`],
+    categories: ensureArray(body.category)
+  };
+
+  try {
+    await api.editOffer(id, offerData);
+    res.redirect(`/my`);
+  } catch (error) {
+    res.redirect(`/offers/edit/${id}?error=${encodeURIComponent(error.response.data)}`);
+  }
 });
 
 offersRouter.get(`/:id`, async (req, res) => {
-  const { id } = req.params;
+  const {id} = req.params;
+  const {error} = req.query;
   const offer = await api.getOffer(id, true);
-  res.render(`offers/ticket`, { offer });
+  res.render(`offers/ticket`, {offer, id, error});
+});
+
+offersRouter.post(`/:id/comments`, async (req, res) => {
+  const {id} = req.params;
+  const {comment} = req.body;
+
+  try {
+    await api.createComment(id, {text: comment});
+    res.redirect(`/offers/${id}`);
+  } catch (error) {
+    res.redirect(`/offers/${id}?error=${encodeURIComponent(error.response.data)}`);
+  }
 });
 
 module.exports = offersRouter;
